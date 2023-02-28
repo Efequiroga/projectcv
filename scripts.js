57ae33090bbc08afac3d735fb6f7825a135c4b17
Federico Quiroga <fedequiroga94@gmail.com>
2023-02-28 01:39:52 -0300

    Primera interaccion con git, inicio

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+const boton = document.getElementById("boton")
+const nombre = document.getElementById("nombre")
+const gmail = document.getElementById("gmail")
+const telefono = document.getElementById("telefono")
+const edad = document.getElementById("edad")
+
+const generarUsuario = async () => {
+    
+    const url = 'https://randomuser.me/api/';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+    console.log(datos)
+
+    foto.src = datos.picture.medium;    
+    nombre.textContent = datos.name.first + ' ' + datos.name.last;
+    gmail.textContent = datos.email;
+    telefono.textContent = datos.phone;
+    edad.textContent = datos.dob.age;
+}
+
+
+boton.addEventListener("click", generarUsuario);
+document.addEventListener("DOMContentLoaded", generarUsuario);
